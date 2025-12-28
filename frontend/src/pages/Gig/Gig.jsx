165c3d35fd99eca69f6ea3d54b34b55bca3e62fc
Anand-Kumar-63 <anandks8013@gmail.com>
2025-12-28 05:49:04 +0530

    Gig Page Backend Integration

--- frontend/src/pages/Gig/Gig.jsx
+++ frontend/src/pages/Gig/Gig.jsx
@@ -1,12 +1,11 @@
-import React, { useEffect } from "react";
-import { useLocation, useParams } from "react-router";
+import React from "react";
+import { Link, useParams } from "react-router";
 import { FaStar } from "react-icons/fa6";
 import { GoClock } from "react-icons/go";
 import { TfiReload } from "react-icons/tfi";
 import { FiCheckSquare } from "react-icons/fi";
 import { FaRegThumbsUp } from "react-icons/fa";
 import { FaRegThumbsDown } from "react-icons/fa";
-import Footer from "../../Components/Footer/Footer";
 import { IoSyncCircleOutline } from "react-icons/io5";
 import { SlSocialInstagram } from "react-icons/sl";
 import { FaFacebook } from "react-icons/fa6";
@@ -17,7 +16,6 @@ import { CiGlobe } from "react-icons/ci";
 import { AiTwotoneDollarCircle } from "react-icons/ai";
 import axios from "axios";
 import { reviews } from "./Data";
-
 import {
   Carousel,
   CarouselContent,
@@ -25,12 +23,9 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
-
-// import { array } from "./Data";
 import { info } from "./Data";
 import { Card, CardContent } from "@/Components/ui/card";
-import { useEffectEvent } from "react";
-import { useQueries, useQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 
 const arr = [1, 2, 3, 4, 5];
 const categoriesData = [
@@ -43,26 +38,9 @@ const categoriesData = [
 
 const Gig = () => {
   const param = useParams();
-  const array = [
-    {
-      head: "Propmt Writing",
-      Icon: <FiCheckSquare />,
-    },
-    {
-      head: "Additional desgin",
-      Icon: <FiCheckSquare />,
-    },
-    {
-      head: "Art delivery",
-      Icon: <FiCheckSquare />,
-    },
-    {
-      head: "Image upscaling",
-      Icon: <FiCheckSquare />,
-    },
-  ];
-
   const id = param.gigId;
+  
+  // to get the gigdata from the Backend using the tanstack query 
   const { isPending, error, data } = useQuery({
     queryKey: ["get-querydata"],
     queryFn: async () => {
@@ -75,33 +53,36 @@ const Gig = () => {
       return response?.data;
     },
   });
+  console.log(data);
   const gigdata = data?.data;
   console.log(gigdata);
-  // const userId = data?.data?.userId;
-  // const {data:data2} = useQuery({
-  //   queryKey:["get-user"],
-  //   queryFn:async()=>{
-  //      const response = await axios.get(`http://localhost:3000/user/getuser/${userId}`,{withCredentials:true});
-  //      return response.data;
-  //   }
-  // })
-  // console.log(data2);
-  // console.log(
-  //   Array.from({
-  //     length: Math.round(gigdata.totalStar/gigdata.starNumber),
-  //   })
-  // );
-  return (
+
+  // to get the current user from the localhost
+  const currentuser = localStorage.getItem("currentUser");
+  const parsedUser = JSON.parse(currentuser);
+  console.log(parsedUser);
+
+  // to calculate the date
+  const date = new Date(gigdata?.createdAt);
+  const now = new Date();
+  const difference = now - date;
+  // this difference is going to be millisecond so divide it by milliseconds in 24 hours
+  const differenceInDays = Math.floor(difference / (1000 * 60 * 60 * 24));
+
+  // Array for the stars
+  const stararray = Array.from({
+    length: Math.round(gigdata?.totalStar / gigdata?.starNumber),
+  });
+
+  return !isPending ? (
     <div className="">
       <div className="py-10 relative h-[600px] bg-gray-100">
         <section className="px-10 space-y-2 w-[800px] absolute left-56 ">
           <span>{`FIVERR > GRAPHICS & DESIGN`}</span>
-          <h1 className="text-2xl text-black font-bold">
-            I will create AI artwork for you
-          </h1>
+          <h1 className="text-2xl text-black font-bold">{gigdata.title}</h1>
 
           <span className="space-y-2 flex flex-row items-center space-x-2">
-            <h1 className="mt-2">username</h1>
+            <h1 className="mt-2">{parsedUser.username}</h1>
           </span>
 
           <Carousel
@@ -111,11 +92,17 @@ const Gig = () => {
             className="w-full max-w-2xl h-[440px]"
           >
             <CarouselContent>
-              {gigdata.Image.map(( item , index) => (
+              {gigdata.Image.map((img, index) => (
                 <CarouselItem key={index} className="">
                   <div className="p-1 flex justify-center">
-                    <Card className="bg-green-400 h-[440px] w-[580px]">
-                      <img src={item} alt="Girl in a jacket" width="500" height="600"></img>
+                    <Card className="h-[440px] w-[580px] flex justify-center items-center">
+                      <CardContent className="flex items-center justify-center p-1">
+                        <img
+                          src={img}
+                          alt="Alt"
+                          className="object-cover rounded-xl"
+                        ></img>
+                      </CardContent>
                     </Card>
                   </div>
                 </CarouselItem>
@@ -127,41 +114,41 @@ const Gig = () => {
         </section>
 
         {/* payment card */}
-        <div className="max-w-[360px] max-h-[300px] border p-2 flex flex-col space-y-2 fixed top-30 right-64 z-10">
+        <div className="max-w-[360px] bg-transparent rounded-xl max-h-[300px] border p-2 flex flex-col space-y-2 fixed top-30 right-64 z-10">
           <span className="flex flex-row items-center justify-between">
-            <h1>1 AI generated artwork</h1>
-            <p>$59.90</p>
+            <h1>{gigdata.title}</h1>
+            <p>${gigdata.price}</p>
           </span>
 
           <span>
-            <p className="text-sm leading-relaxed">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum
-              magni necessitatibus suscipit pariatur illo nobis magnam, culpa
-              non saepe reprehenderit quod hic esse ullam repellendus.
-            </p>
+            <p className="text-sm leading-relaxed">{gigdata.desc}</p>
           </span>
 
           <span className="flex flex-row justify-between">
             <span className="flex flex-row space-x-1 items-center">
               <GoClock />
-              <h1>2 days later</h1>
+              <h1>{differenceInDays} days later</h1>
             </span>
             <span className="flex flex-row space-x-1 items-center">
               <TfiReload />
-              <h1>3 Revisions</h1>
+              <h1>{gigdata.revisionNumber} Revisions</h1>
             </span>
           </span>
 
           <ul>
-            {array.map((item, index) => (
-              <li
-                key={index}
-                className="flex flex-row items-center space-x-1 text-green-500"
-              >
-                {item.Icon}
-                <h1 className="text-gray-700 text-sm">{item.head}</h1>
-              </li>
-            ))}
+            {!isPending ? (
+              gigdata.Features.map((item, index) => (
+                <li
+                  key={index}
+                  className="flex flex-row items-center space-x-1 text-green-500"
+                >
+                  {<FiCheckSquare />}
+                  <h1 className="text-gray-700 text-sm">{item}</h1>
+                </li>
+              ))
+            ) : (
+              <div>loading</div>
+            )}
           </ul>
 
           <button className="text-white bg-green-400 py-1 rounded">
@@ -170,28 +157,10 @@ const Gig = () => {
         </div>
       </div>
 
-      <div className="mx-[260px] w-[740px]">
-        <h1>About this page</h1>
+      <div className="mx-[260px] w-[740px] m-2">
+        <h1 className="text-2xl">About this page</h1>
         <p className="text-xl/0.8 leading-relaxed text-gray-500">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat,
-          fuga. Totam voluptate ex veniam debitis consequuntur voluptatibus quis
-          magnam consectetur quos, et, expedita sit temporibus vero delectus.
-          Vero sequi repudiandae iste quaerat magni, impedit dolor in aperiam
-          ratione qui vitae aspernatur nobis omnis. Laboriosam, deleniti
-          adipisci quae officiis quis consequuntur necessitatibus vero ipsam
-          labore, accusantium dolorem!
-          <br />
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore
-          incidunt perferendis laborum impedit a explicabo aperiam rerum eaque
-          reiciendis. Eius aperiam aut aliquid voluptas? Numquam accusantium
-          assumenda a in repellat quia aliquid enim? Eaque nostrum porro facilis
-          fugit aperiam corrupti est dolorum consectetur doloribus nesciunt
-          consequuntur rerum laborum consequatur id architecto nulla qui, iste
-          explicabo ab vel odio ducimus quia! Veniam quaerat nobis facere eum. A
-          earum ipsum dolore delectus corrupti nemo modi tenetur quod adipisci
-          repellendus. Quod accusamus placeat sed similique magnam provident
-          facilis, fuga eius est assumenda quo dolorum excepturi, optio
-          repudiandae, vitae ducimus cum ad ab dignissimos!
+          {gigdata.desc}
         </p>
       </div>
 
@@ -206,17 +175,19 @@ const Gig = () => {
             />
           </span>
           <span className="flex flex-col space-y-1 max-w-[200px]">
-            <h1>Anna Bell</h1>
+            <h1>{parsedUser.username}</h1>
             <ul className="flex flex-row space-x-1 text-amber-300">
-              <FaStar />
-              <FaStar />
-              <FaStar />
-              <FaStar />
-              <FaStar />
+              {stararray.map((_,index)=>{
+                return(
+                   <FaStar key={index}/>
+                )
+              })}
             </ul>
-            <button className="bg-white border border-gray-300 rounded-2xl px-2 w-[120px]">
+            <Link to={"/Chat"}>
+            <button className="bg-white border border-gray-300 px-2">
               Contact Me!
             </button>
+            </Link>
           </span>
         </span>
 
@@ -235,11 +206,6 @@ const Gig = () => {
           <p className="text-gray-500 text-12 mt-4">
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus
             modi vitae necessitatibus ullam dolor enim dolores tenetur unde ut
-            nobis debitis repudiandae molestias quae sequi, fugiat minus soluta
-            ratione, itaque cum, quod sunt corrupti? Qui ea minima architecto
-            quis? Consequuntur eveniet dolore tempore cupiditate sapiente iste
-            tenetur id temporibus maxime officia mollitia, corrupti voluptas,
-            ipsam omnis nam hic sunt placeat.
           </p>
         </div>
 
@@ -290,6 +256,9 @@ const Gig = () => {
           })}
         </div>
       </div>
+
+
+       {/* footer */}
       <div className="flex flex-col space-x-50 mx-10 p-10 my-6 select-none">
         <div className="flex flex-row justify-between mx-26 cursor-pointer">
           {arr.map((item, index) => {
@@ -331,6 +300,8 @@ const Gig = () => {
         </section>
       </div>
     </div>
+  ) : (
+    <div>Loading</div>
   );
 };
 
